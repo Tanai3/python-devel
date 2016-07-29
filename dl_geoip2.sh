d9bbc7c7ed6f6a34f826943098eed45f5e9b6c2f
chayka1206 <ec.chayka@gmail.com>
2016-07-29 13:07:04 +0900

    update

--- dl_geoip2.sh
+++ dl_geoip2.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 wget geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz
-gunzip -v GeoLite2-City.mmdb.gz
+mkdir -v GeoIP
+mv -v GeoLite2-City.mmdb.gz ./GeoIP
+gunzip -v GeoIP/GeoLite2-City.mmdb.gz
